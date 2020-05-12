c2d793463ae04e72b150f296a951ddb6dda52af8
Camilo Davila <camilo7davila@gmail.com>
2020-05-12 17:20:36 -0500

    first commit

---
+++ src/app/home/home-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ChatComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { ChatComponent } from './components/chat/chat.component';
+
+
+@NgModule({
+  declarations: [ChatComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
